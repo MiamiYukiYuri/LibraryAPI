6b30be638ea068c46ef09b5bd02bd27eeab5b635
Mia Stefansdotter <stefansdotter@gmail.com>
2024-01-20 16:47:47 +0100

    added more to loan packages

--- src/main/java/com/example/demo/controllers/LoanController.java
+++ src/main/java/com/example/demo/controllers/LoanController.java
@@ -1,4 +1,46 @@
 package com.example.demo.controllers;
 
+import com.example.demo.services.LoanService;
+import com.example.demo.models.Loan;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+@RestController
+@RequestMapping(value="/api")
 public class LoanController {
+
+    @Autowired
+    LoanService loanService;
+
+    // POST - ADD loan  http://localhost:8080/api/book
+    @PostMapping("/loan")
+    public Loan createLoan(@RequestBody Loan loan) {
+        return loanService.createLoan(loan);
+    }
+
+    // GET ALL loans
+    @GetMapping("/loan")
+    public List<Loan> getAllLoans() {
+        return loanService.getAllLoans();
+    }
+
+    // GET loan by id
+    // {id} = path parameter
+    @GetMapping("/loan/{id}")
+    public Loan getLoanById(@PathVariable String id) {
+        return loanService.getLoanById(id);
+    }
+
+    // PUT - update loan info
+    @PutMapping("/loan")
+    public Loan updateLoan(@RequestBody Loan loan) {
+        return loanService.updateLoan(loan);
+    }
+
+    // DELETE loan
+    @DeleteMapping("/loan/{id}")
+    public void deleteLoan(@PathVariable String id) {
+        loanService.deleteLoan(id);
+    }
 }
